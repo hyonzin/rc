eb561aebd19fa6c696e0ef4a507476d883fd1b50
hyonzin <hyeonjin507@gmail.com>
2022-12-05 14:27:01 +0900

    update 221205

--- setting_my_linux.sh
+++ setting_my_linux.sh
@@ -1,49 +1,24 @@
 #!/bin/bash
 
 say () {
+    echo $@
 	echo $@ >> $DEST
 }
 
-DEST=~/.hyonzinrc
-
-say
-say "#================================"
-say "# hyeonjin added `date '+%F'`"
-say "#================================"
-say
-say "parse_git_branch() {"
-say "  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\\\\1)/'"
-say "}"
-say "export PS1=\"[\e[0;32m\w\e[m]\e[0;33m\\\$(parse_git_branch)\e[m\\n\u@\h$ \""
-say
-say "# Set CLICOLOR if you want Ansi Colors in iTerm2"
-say "export CLICOLOR=1"
-say 
-say "# Set colors to match iTerm2 Terminal Colors"
-say "export TERM=xterm-256color"
-say
-say "alias l='ls -CF'"
-say "alias la='ls -A'"
-say "alias ll='ls -alF'"
-say "alias lh='ls -alFh'"
-say "alias tf='tail -F'"
-say "alias ta='tmux attach'"
-say
-say "#================================"
+curl -L https://raw.githubusercontent.com/hyonzin/rc/master/.bashrc.hyonzin > ~/.bashrc.hyonzin
+curl -L https://raw.githubusercontent.com/hyonzin/rc/master/.git-completion.sh > ~/.git-completion.sh
 
 DEST=~/.bashrc
+echo [to $DEST]
 say
 say "#================================"
-say "# hyeonjin added `date '+%F'`"
-say "#================================"
-say
-say "source ~/.hyonzinrc"
-say
+say "# hyonzin/rc `date '+%F'`"
+say "source ~/.bashrc.hyonzin"
 say "#================================"
 
 DEST=~/.vimrc
-
-say "\" hyeonjin added `date '+%F'`"
+echo [to $DEST]
+say "\" hyonzin/rc `date '+%F'`"
 say "set nu"
 say "set nocp"
 say "set softtabstop=4"
@@ -55,14 +30,14 @@ say "set cindent"
 say "syntax on"
 say "colorscheme elflord"
 
+# git global setting
 git config --global user.name  "hyonzin"
 git config --global user.email "hyeonjin507@gmail.com"
 git config --global push.default current
 git config --global http.postBuffer 524288000
 
-
+# generate ssh key
 if [ ! -f ~/.ssh/id_rsa ]; then
 	mkdir -p ~/.ssh
 	ssh-keygen -t rsa -q -N "" -f ~/.ssh/id_rsa
 fi
-
